1aeb78eb042b836915fcd90444755f077f2db5a1
Bumja12 <113402190+Bumja12@users.noreply.github.com>
2024-08-30 14:58:14 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    before, answer = '', ''
+    for char in s:
+        if char != ' ' and not before or before == ' ':
+            answer += char.upper()
+        else:
+            answer += char.lower()
+        before = char
+            
+    return answer
