5a280cb4f66edf1b095653947c286bbc1e9f5c04
Bumja12 <113402190+Bumja12@users.noreply.github.com>
2024-09-02 19:28:13 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,6 +1,6 @@
 def solution(n, words):
-    used_word = set()
     last_word = words[0][0]
+    used_word = set()
 
     for i, word in enumerate(words):
         if word not in used_word and last_word[-1] == word[0]:
@@ -10,3 +10,7 @@ def solution(n, words):
             return [i % n + 1, i // n + 1]
 
     return [0, 0]
+
+# last_word[-1] == word[0] 조건을 만족하기 위해 last_word 를 words[0][0]로 초기화
+# 사용한 단어는 set을 사용하여 확인
+# 반복문을 빠져나온다면 정상적으로 마무리된 것으로 [0, 0] 리턴
