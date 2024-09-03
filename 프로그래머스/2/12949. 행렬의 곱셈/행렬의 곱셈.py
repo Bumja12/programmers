05aa7c159734bf05a502aca954b9ac4a577ee99a
Bumja12 <113402190+Bumja12@users.noreply.github.com>
2024-09-03 13:27:25 +0900

    [level 2] Title: 행렬의 곱셈, Time: 51.79 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,12 +1,4 @@
 def solution(arr1, arr2):
-    answer = []
+    arr2_transposed = list(zip(*arr2))
     
-    for i, arr1_row in enumerate(arr1):
-        result_row = []
-        for j in range(len(arr2[0])):
-            arr2_column = [t[j] for t in arr2]
-            element = sum(([a * b for a, b in zip(arr1_row, arr2_column)]))
-            result_row.append(element)
-        answer.append(result_row)
-        
-    return answer
+    return [[sum(a * b for a, b in zip(row1, col2)) for col2 in arr2_transposed] for row1 in arr1]
