623d916a8b836559269ec72745c6d17e0fee9bf6
Bumja12 <113402190+Bumja12@users.noreply.github.com>
2024-09-04 18:47:45 +0900

    [level 2] Title: 프로세스, Time: 0.09 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,4 +1,4 @@
-from collections import Counter, deque
+from collections import Counter
 
 def solution(priorities, location):
     counter = Counter(priorities)
@@ -16,3 +16,8 @@ def solution(priorities, location):
                 if counter[p] == 0:
                     del counter[maxx]
                     maxx = max(counter.keys())
+
+# 기존에는 queue로 처리하려 했으나, 오히려 따로 카운팅을 해줘야하는 문제로 푸는 방식 변경
+# - 근데 다른 사람 코드를 보니 tuple로 index와 같이 저장해서 사용하는 경우가 있었음. 천재..
+# 매번 max를 호출하면 비효율적으로 0이 됐을 때만 기존 값 제거 후 max 호출
+# index 와 location이 같아지는 순간 0으로 바꾼 실행된 프로세스의 수를 리턴
