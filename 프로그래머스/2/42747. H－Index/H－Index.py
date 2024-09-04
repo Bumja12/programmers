1651aa377df532934e7f2b8b2dfa22d06f4aaa9d
Bumja12 <113402190+Bumja12@users.noreply.github.com>
2024-09-04 13:28:48 +0900

    [level 2] Title: H-Index, Time: 0.21 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -4,3 +4,6 @@ def solution(citations):
         if i+1 > citation:
             return i
     return len(citations)
+
+# h가 인용수라는 점에 꽂혀서 논문의 수(i) 가 아닌 인용수(citation)를 리턴하여 해결하지 못하는 문제가 있었음
+# [6, 4, 1, 0] 에서 h는 2가 되는데, citation 을 리턴하면 4를 리턴하게 됨
